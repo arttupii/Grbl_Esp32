fa8ec2bf1ada85940d941954c727edfe1a9fbb28
SourceRabbit <79667074+SourceRabbit@users.noreply.github.com>
2021-03-15 23:31:14 +0200

    Update sourcerabbit_4axis_cnc_motherboard.h

--- Grbl_Esp32/src/Machines/sourcerabbit_4axis_cnc_motherboard.h
+++ Grbl_Esp32/src/Machines/sourcerabbit_4axis_cnc_motherboard.h
@@ -9,7 +9,7 @@
 #define N_AXIS 4
 
 
-#define DEFAULT_DIRECTION_INVERT_MASK 	4
+#define DEFAULT_DIRECTION_INVERT_MASK 		4
 #define DEFAULT_INVERT_LIMIT_PINS 		0
 #define DEFAULT_INVERT_PROBE_PIN 		0
 
@@ -29,23 +29,23 @@
 
 // SPEEDS AND ACCELLERATIONS
 #define DEFAULT_X_STEPS_PER_MM 			400.0
-#define DEFAULT_X_MAX_RATE 				5000.0  // mm/min
+#define DEFAULT_X_MAX_RATE 			5000.0  // mm/min
 #define DEFAULT_X_ACCELERATION 			80.0
 
 #define DEFAULT_Y_STEPS_PER_MM 			400.0
-#define DEFAULT_Y_MAX_RATE 				5000.0  // mm/min
+#define DEFAULT_Y_MAX_RATE 			5000.0  // mm/min
 #define DEFAULT_Y_ACCELERATION 			80.0
 
 #define DEFAULT_Z_STEPS_PER_MM 			400.0
-#define DEFAULT_Z_MAX_RATE 				2500.0  // mm/min
+#define DEFAULT_Z_MAX_RATE 			2500.0  // mm/min
 #define DEFAULT_Z_ACCELERATION 			60.0
 
 #define DEFAULT_A_STEPS_PER_MM 			400.0	// Steps / Degree
-#define DEFAULT_A_MAX_RATE 				3600.0  // degrees/min
+#define DEFAULT_A_MAX_RATE 			3600.0  // degrees/min
 #define DEFAULT_A_ACCELERATION 			60.0
 
 
-// SourceRabbit CNC Motherboard v1.2 Pinout
+// SourceRabbit CNC Motherboard v1.1 Pinout
 #define X_STEP_PIN              GPIO_NUM_0
 #define X_DIRECTION_PIN         GPIO_NUM_33
 #define Y_STEP_PIN              GPIO_NUM_25
