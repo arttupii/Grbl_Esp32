89e7d718b0f297c58dbaa23b44c0f9be89db8add
Nikos Siatras <nsiatras@gmail.com>
2021-03-16 11:46:37 +0200

    Machine added

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -4,5 +4,11 @@
 // one or more machine definition files as described below.
 
 
-#include "Machines/sourcerabbit_4axis_cnc_motherboard.h"
+// To build for the SourceRabbit 4Axis CNC Motherboard
+// uncomment the following line
+//#include "Machines/sourcerabbit_4axis_cnc_motherboard.h"
+
+// To build for the SourceRabbit Micro Mill 4 Axis
+// uncomment the following line
+#include "sourcerabbit_MicroMill_4Axis"
 
